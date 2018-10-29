868387201a37e25add2c98e2a79e8e2e929db47d
worknaja <nockandriod@gmail.com>
2018-10-29 16:11:20 +0700

    code all Fragment

--- app/src/main/java/com/example/nsx/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nsx/myapplication/MainActivity.java
@@ -9,5 +9,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //        Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment())
+                    .commit();
+        }
     }
 }
